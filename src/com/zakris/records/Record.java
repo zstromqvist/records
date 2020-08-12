9d4729aa350b37036bb476640e618c23ac6b5269
zstromqvist <zakrisstromqvist@gmail.com>
2020-08-12 21:58:49 +0200

    add base searching and record class

--- src/com/zakris/records/Record.java
+++ src/com/zakris/records/Record.java
@@ -1,7 +1,71 @@
 package com.zakris.records;
 
 public class Record {
-    public static void hello() {
-        System.out.println("hello records");
+    private String artist;
+    private String name;
+    private int year;
+    private int pressYear = 0;
+    private String index;
+    private String notes = "";
+
+    // constructor
+    public Record(String artist, String name, int year, int pressYear, String index, String notes) {
+        this.artist = artist;
+        this.name = name;
+        this.year = year;
+        this.pressYear = pressYear;
+        this.index = index;
+        this.notes = notes;
+    }
+
+    // setters
+    public void setArtist(String artistName) {
+        this.artist = artistName;
+    }
+
+    public void setName(String albumName) {
+        this.name = albumName;
+    }
+
+    public void setYear(int albumYear) {
+        this.year = albumYear;
+    }
+
+    public void setPressYear(int albumPressYear) {
+        this.pressYear = albumPressYear;
+    }
+
+    public void setIndex(String albumIndex) {
+        this.index = albumIndex;
+    }
+
+    public void setnotes(String albumNotes) {
+        this.notes = albumNotes;
+    }
+    
+
+    // getters
+    public String getArtist() {
+        return this.artist;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public int getPressYear() {
+        return this.pressYear;
+    }
+
+    public String getIndex() {
+        return this.index;
+    }
+
+    public String getNotes() {
+        return this.notes;
     }
 }
--- src/com/zakris/records/Records.java
+++ src/com/zakris/records/Records.java
@@ -1,28 +1,52 @@
 package com.zakris.records;
 
-import java.util.Scanner; 
+import java.io.*;
 
 public class Records {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        // General stuff
+        String fileName = "db.csv";
+
+        System.out.println("Welcome! What do you want to do?\n 1: Search records\n 2: Add record(s)");
+        System.out.print("Choice: ");
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int choice = Integer.parseInt(input.readLine());
+
+        if (choice == 1) {
+            System.out.println("What do you want to search for?\n 1: Artist\n 2: Album\n 3: Year");
+            BufferedReader choiceInput = new BufferedReader(new InputStreamReader(System.in));
+            int searchChoice = Integer.parseInt(choiceInput.readLine());
+
+            System.out.println("Please enter what you're looking for?");
+            System.out.print("Choice: ");
+            BufferedReader searchInput = new BufferedReader(new InputStreamReader(System.in));
+            String searchWord = searchInput.readLine();
+            searchInput.close();
+
+            BufferedReader inFile = new BufferedReader(new FileReader(fileName));
+            String line = "";
+            int counter = 0;
+            while ((line = inFile.readLine()) != null) {
+                
+
+                String [] t = line.split(",");
+
+                if (t[searchChoice + 1].equals(searchWord)) {
+                    System.out.println(t[2] + ", " + t[3] + ", " + t[4] + ", Index: " + t[1]);
+                    counter++;
+                }
+            }
+            if (counter == 0) {
+                System.out.println("Nothing was found!");
+            }
+            
+            inFile.close();
+        } else {
+            System.out.println("You chose 2");
+        }
+
         
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Artist name:");
-        String artist = scan.nextLine();
-        
-        System.out.println("Album name:");
-        String album = scan.nextLine();
-
-        System.out.println("Year:");
-        int year = scan.nextInt();
-
-        System.out.println("Notes:");
-        String notes = scan.nextLine();
-
-        System.out.println("The artist is: " + artist);
-        System.out.println("The album is: " + album);
-        System.out.println("The year is: " + year);
-        System.out.println(notes);
-
-        scan.close();
     }
 }
