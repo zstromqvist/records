f5a062e592609b6cf6274c2b778652d1a994cc6a
zstromqvist <zakrisstromqvist@gmail.com>
2020-08-13 22:38:06 +0200

    add format to main and run formatiser

--- src/com/zakris/records/Records.java
+++ src/com/zakris/records/Records.java
@@ -9,56 +9,60 @@ public class Records {
         // General stuff
         String fileName = "db.csv";
 
-        System.out.println("Welcome! What do you want to do?\n 1: Search records\n 2: Add record(s)\n 3: Pick a record for me!");
+        System.out.println(
+                "Welcome! What do you want to do?\n 1: Search records\n 2: Add record(s)\n 3: Pick a record for me!");
         System.out.print("Choice: ");
 
-        BufferedReader input = new BufferedReader(
-            new InputStreamReader(System.in));
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         int choice = Integer.parseInt(input.readLine());
 
         if (choice == 1) {
-            System.out.println("What do you want to search for?\n 1: Artist\n 2: Album\n 3: Year");
-            BufferedReader choiceInput = new BufferedReader(
-                new InputStreamReader(System.in));
-            int searchChoice = Integer.parseInt(choiceInput.readLine());
-
-            System.out.println("Please enter what you're looking for?");
+            System.out.println("What do you want to search for?\n 1: Artist (Default)\n 2: Album\n 3: Year");
             System.out.print("Choice: ");
-            BufferedReader searchInput = new BufferedReader(
-                new InputStreamReader(System.in));
+            BufferedReader choiceInput = new BufferedReader(new InputStreamReader(System.in));
+            String inputChoice = choiceInput.readLine();
+            int searchChoice;
+
+            if (inputChoice.equals("")) {
+                searchChoice = 1;
+            } else {
+                searchChoice = Integer.parseInt(inputChoice);
+            }
+
+            System.out.print("Please enter what you're looking for: ");
+            BufferedReader searchInput = new BufferedReader(new InputStreamReader(System.in));
             String searchWord = searchInput.readLine();
             searchInput.close();
 
-            BufferedReader inFile = new BufferedReader(
-                new FileReader(fileName));
+            BufferedReader inFile = new BufferedReader(new FileReader(fileName));
             String line = "";
             int counter = 0;
+            int counter2 = 0;
             while ((line = inFile.readLine()) != null) {
-                
 
-                String [] recordArray = line.split(",");
+                String[] recordArray = line.split(",");
 
-                if (recordArray[searchChoice + 1].equals(searchWord)) {
-                    System.out.println(recordArray[2] + ", " + recordArray[3] + ", " + recordArray[4] + ", Index: " + recordArray[1]);
+                if (recordArray[searchChoice + 1].toLowerCase().contains(searchWord.toLowerCase()) && counter2 > 0) {
+                    System.out.println(recordArray[2] + ", " + recordArray[3] + ", " + recordArray[4] + ", "
+                            + recordArray[5] + ", Index: " + recordArray[1]);
                     counter++;
                 }
+                counter2++;
             }
             if (counter == 0) {
                 System.out.println("Nothing was found!");
             }
-            
-            inFile.close();
-        } else if(choice == 2){
 
-            PrintWriter outstream = new PrintWriter(
-                new BufferedWriter(
-                    new FileWriter(fileName, true)));
-            
-            int currentLastDbId = getLastDbId(fileName); 
+            inFile.close();
+        } else if (choice == 2) {
+
+            PrintWriter outstream = new PrintWriter(new BufferedWriter(new FileWriter(fileName, true)));
+
+            int currentLastDbId = getLastDbId(fileName);
             int counter = 1;
 
             while (true) {
-                
+
                 int recordId = currentLastDbId + counter;
                 counter++;
 
@@ -77,31 +81,30 @@ public class Records {
                 System.out.print("Year of album press: ");
                 int recordPressYear = Integer.parseInt(recordInput.readLine());
 
+                System.out.println("Format\n 1: Vinyl\n 2: CD");
+                System.out.print("1 or 2: ");
+                int recordFormatInput = Integer.parseInt(recordInput.readLine());
+                String recordFormat = "";
+
+                if (recordFormatInput == 1) {
+                    recordFormat = "Vinyl";
+                } else {
+                    recordFormat = "CD";
+                }
+
                 System.out.print("Index: ");
                 String recordIndex = recordInput.readLine();
 
                 System.out.print("Notes: ");
                 String recordNotes = recordInput.readLine();
 
-                Record newRecord = new Record(
-                        recordId, 
-                        recordArtist, 
-                        recordName, 
-                        recordYear, 
-                        recordPressYear, 
-                        recordIndex,
-                        recordNotes);
+                Record newRecord = new Record(recordId, recordIndex, recordArtist, recordName, recordYear, recordFormat,
+                        recordPressYear, recordNotes);
 
-                outstream.println(
-                    newRecord.getId() + "," + 
-                    newRecord.getIndex() + "," + 
-                    newRecord.getArtist() + "," + 
-                    newRecord.getName() + "," + 
-                    newRecord.getYear() + "," + 
-                    newRecord.getPressYear() + "," + 
-                    newRecord.getNotes()
-                    ); 
-                
+                outstream.println(newRecord.getId() + "," + newRecord.getIndex() + "," + newRecord.getArtist() + ","
+                        + newRecord.getName() + "," + newRecord.getYear() + "," + newRecord.getFormat() + ","
+                        + newRecord.getPressYear() + "," + newRecord.getNotes());
+
                 System.out.print("Do you want to add another one? y/n: ");
                 String addMore = recordInput.readLine();
 
@@ -110,19 +113,19 @@ public class Records {
                     break;
                 }
             }
-            
+
         } else {
             Random r = new Random();
             int randomRecordId = r.nextInt((getLastDbId(fileName) - 1) + 1) + 1;
-            BufferedReader inFile = new BufferedReader(
-                new FileReader(fileName));
+            BufferedReader inFile = new BufferedReader(new FileReader(fileName));
 
             String line = "";
             while ((line = inFile.readLine()) != null) {
 
-                String [] recordArray = line.split(",");
+                String[] recordArray = line.split(",");
                 if (Integer.toString(randomRecordId).equals(recordArray[0])) {
-                    System.out.println(recordArray[2] + ", " + recordArray[3] + ", " + recordArray[4] + ", Index: " + recordArray[1]);
+                    System.out.println(recordArray[2] + ", " + recordArray[3] + ", " + recordArray[4] + ", "
+                            + recordArray[5] + ", Index: " + recordArray[1]);
                 }
             }
             inFile.close();
