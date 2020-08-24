f203753438a34e00e9389ccbc990ddda79e8737b
zstromqvist <zakrisstromqvist@gmail.com>
2020-08-24 20:05:08 +0200

    make list by artist menu look nicer and cleaner

--- src/com/zakris/records/ListByArtist.java
+++ src/com/zakris/records/ListByArtist.java
@@ -3,7 +3,6 @@ package com.zakris.records;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -24,35 +23,28 @@ public class ListByArtist extends JFrame {
         final DatabaseConnection con = new DatabaseConnection();
         final String[] artists = con.getArtists(con.getLastDbId());
         final int textFieldSize = 18;
-        int textLabelSize = 16;
+        this.setTitle("List by artist");
 
-        int inputLabelX = 35;
-        int inputFieldX = 215;
+        int inputFieldX = 80;
         int inputY = 15;
 
         setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        setBounds(500, 100, 450, 380);
+        setBounds(500, 100, 450, 150);
         setResizable(false);
         panel = new JPanel();
         panel.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(panel);
         panel.setLayout(null);
 
-        final JLabel artistLabel = new JLabel("Artist");
-        artistLabel.setFont(new Font("Tahoma", Font.PLAIN, textLabelSize));
-        artistLabel.setBounds(inputLabelX, inputY, 200, 30);
-        panel.add(artistLabel);
-
         final JComboBox<String> artist = new JComboBox<>(artists);
         artist.setFont(new Font("Tahoma", Font.PLAIN, textFieldSize));
-        artist.setBounds(inputFieldX, inputY, 200, 30);
+        artist.setBounds(inputFieldX, inputY, 300, 30);
         panel.add(artist);
 
         searchButton = new JButton("Search!");
         searchButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
-        searchButton.setBounds(150, 310, 150, 30);
+        searchButton.setBounds(150, 60, 150, 30);
         panel.add(searchButton);
-
 
         searchButton.addActionListener(new ActionListener() {
             @Override
